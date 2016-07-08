47a692910b72fbce06655f0461bf188e5a1d44c1
이주영 <stylejy@gmail.com>
2016-07-08 19:12:45 +0100

    Initial commit from local git

---
+++ Pray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pray
+//
+//  Created by 이주영 on 08/07/2016.
+//  Copyright © 2016 이주영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
