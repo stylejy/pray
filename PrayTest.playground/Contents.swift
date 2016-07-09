a1b8735a4e5c6ff992df9e6dfdeeecffb0f466c8
이주영 <stylejy@gmail.com>
2016-07-09 20:47:22 +0100

    Sample database model

--- PrayTest.playground/Contents.swift
+++ PrayTest.playground/Contents.swift
@@ -1,24 +1,64 @@
 //: Playground - noun: a place where people can play
 //DB test
 
-class GroupList {
-    var group: [String] = []
+class GroupModel {
+    var groupName: String = ""
+    var groupMembers: [String: String] = [:]
     
-    func addGroup(name: String) {
-        group.append(name)
+    func returnGroupName() -> String {
+        return groupName
     }
     
-    func returnGroup() -> [String] {
-        return group
+    func returnGroupMembers() -> [String: String] {
+        return groupMembers
+    }
+    
+    func setGroupName(inputName: String) {
+        groupName = inputName
+    }
+    
+    func setGroupMemberName(inputName: String) {
+        groupMembers[inputName] = ""
+    }
+    
+    func setGroupMemberPray(inputName: String, inputPray: String) {
+        groupMembers.updateValue(inputPray, forKey: inputName)
     }
 }
 
-let result = GroupList()
+class GroupManagement {
+    var groupList: [GroupModel] = []
+    
+    func addGroup(inputGroupName: String) {
+        let newGroup = GroupModel()
+        newGroup.setGroupName(inputGroupName)
+        groupList.append(newGroup)
+    }
+    
+    func returnGroupList() -> [GroupModel] {
+        return groupList
+    }
+    
+    func addMember(inputMemberName: String, inputGroupNum: Int) {
+        let selectedGroup = groupList[inputGroupNum]
+        selectedGroup.setGroupMemberName(inputMemberName)
+    }
+}
 
+let result = GroupManagement()
 result.addGroup("킹스크로스 한인교회")
 result.addGroup("킹스 기도모임")
+result.addGroup("런던대 연합 기도모임")
 
-result.returnGroup()
+result.addMember("이주영", inputGroupNum: 0)
+result.addMember("윤지훈", inputGroupNum: 0)
+
+
+result.returnGroupList()[0]
+
+
+
+
 
 
 
